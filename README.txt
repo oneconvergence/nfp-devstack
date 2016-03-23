8a19e9647d0e52a554d784008fd63fb25e8925bd
Hemanth Ravi <hemanth.ravi@oneconvergence.com>
2016-03-23 02:20:05 -0700

    Liberty nfp devstack.

--- README.txt
+++ README.txt
@@ -1,22 +1,22 @@
 Install stable/liberty devstack with GBP.
 
-    1. git clone https://git.openstack.org/openstack-dev/devstack -b master
+    1. git clone https://git.openstack.org/openstack-dev/devstack -b stable/liberty
 
     2. cd devstack
 
-    3. wget https://raw.githubusercontent.com/group-policy/gbp-devstack/master/gbp-patch.sh
+    3. wget https://raw.githubusercontent.com/group-policy/gbp-devstack/liberty/gbp-patch.sh
 
     4. chmod +x gbp-patch.sh
 
     5. ./gbp-patch.sh
   
-    6. wget https://raw.githubusercontent.com/oneconvergence/nfp-devstack/master/nfp-patch.sh
+    6. wget https://raw.githubusercontent.com/oneconvergence/nfp-devstack/liberty/nfp-patch.sh
 
     7. chmod +x nfp-patch.sh
 
     8. ./nfp-patch.sh 
 
-    9. edit the local.conf
+    9.  edit local.conf
 
     10. ./stack.sh
 
--- nfp-patch.sh
+++ nfp-patch.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 set -x
 rm -rf local.*
-wget https://raw.githubusercontent.com/oneconvergence/nfp-devstack/master/local.conf
-wget https://raw.githubusercontent.com/oneconvergence/nfp-devstack/master/lib/nfp -P lib/
+wget https://raw.githubusercontent.com/oneconvergence/nfp-devstack/liberty/local.conf
+wget https://raw.githubusercontent.com/oneconvergence/nfp-devstack/liberty/lib/nfp -P lib/
 sed -i 's/source $TOP_DIR\/lib\/dstat/source $TOP_DIR\/lib\/dstat\nsource $TOP_DIR\/lib\/nfp/g' stack.sh
 sed -i "s/install_gbpservice/install_gbpservice\nconfigure_nfp_firewall/g" stack.sh
 sed -i 's/# Restore\/close logging file descriptors/nfp_setup $TOP_DIR $ConfiguratorQcow2Image $VyosQcow2Image\n# Restore\/close logging file descriptors/g' stack.sh
-
-sed -i "s/export OS_USER_DOMAIN_ID=default/#export OS_USER_DOMAIN_ID=default/g"  stack.sh
-sed -i "s/export OS_PROJECT_DOMAIN_ID=default/#export OS_PROJECT_DOMAIN_ID=default/g"  stack.sh
-
-
 
