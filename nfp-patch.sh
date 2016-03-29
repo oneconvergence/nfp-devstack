b774357d9d90b4a2398c021c25ed75312eec6581
“Dhuldev-Valekar” <dhuldev.valekar@oneconvergence.com>
2016-03-29 22:12:37 +0530

    Commited changes related with mitaka release

--- nfp-patch.sh
+++ nfp-patch.sh
@@ -5,6 +5,9 @@ wget https://raw.githubusercontent.com/oneconvergence/nfp-devstack/master/local.
 wget https://raw.githubusercontent.com/oneconvergence/nfp-devstack/master/lib/nfp -P lib/
 sed -i 's/source $TOP_DIR\/lib\/dstat/source $TOP_DIR\/lib\/dstat\nsource $TOP_DIR\/lib\/nfp/g' stack.sh
 sed -i "s/install_gbpservice/install_gbpservice\nconfigure_nfp_firewall/g" stack.sh
+sed -i "s/install_apic_ml2/#install_apic_ml2/g" stack.sh
+sed -i "s/install_aim/#install_aim/g" stack.sh
+sed -i "s/init_aim/#init_aim/g" stack.sh
 sed -i 's/# Restore\/close logging file descriptors/nfp_setup $TOP_DIR $ConfiguratorQcow2Image $VyosQcow2Image\n# Restore\/close logging file descriptors/g' stack.sh
 
 sed -i "s/export OS_USER_DOMAIN_ID=default/#export OS_USER_DOMAIN_ID=default/g"  stack.sh
