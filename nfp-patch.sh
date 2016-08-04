3362f08f6be38d5bd78f9d26eaef14170a1936be
DhuldevValekar <dhuldev.valekar@oneconvergence.com>
2016-08-04 19:04:17 +0530

    Fixed Liberty Install with patch mode

--- nfp-patch.sh
+++ nfp-patch.sh
@@ -9,8 +9,8 @@ wget https://raw.githubusercontent.com/oneconvergence/nfp-devstack/liberty/local
 wget https://raw.githubusercontent.com/oneconvergence/nfp-devstack/liberty/lib/nfp -P lib/
 
 sed -i 's/source $TOP_DIR\/lib\/dstat/source $TOP_DIR\/lib\/dstat\nsource $TOP_DIR\/lib\/nfp/g' stack.sh
-sed -i "s/install_gbpservice/install_gbpservice\n[[ $NFP_DEVSTACK_MODE = advanced ]] && configure_nfp_loadbalancer\n[[ $NFP_DEVSTACK_MODE = advanced ]] && configure_nfp_vpn\n/g" stack.sh
-sed -i "s/start_neutron_service_and_check/[[ $NFP_DEVSTACK_MODE = advanced ]] && configure_nfp_firewall\n    start_neutron_service_and_check/g" stack.sh
+sed -i "s/install_gbpservice/install_gbpservice\n[[ \$NFP_DEVSTACK_MODE = advanced ]] \&\& configure_nfp_loadbalancer\n[[ \$NFP_DEVSTACK_MODE = advanced ]] \&\& configure_nfp_vpn\n/g" stack.sh
+sed -i "s/start_neutron_service_and_check/[[ \$NFP_DEVSTACK_MODE = advanced ]] \&\& configure_nfp_firewall\n    start_neutron_service_and_check/g" stack.sh
 sed -i 's/# Restore\/close logging file descriptors/nfp_setup\n# Restore\/close logging file descriptors/g' stack.sh
 sed -i 's/merge_config_group \$TOP_DIR\/local.conf post-config/merge_config_group \$TOP_DIR\/local.conf post-config\n[[ $ENABLE_NFP = False ]] \&\& merge_config_group \$TOP_DIR\/local.conf post-config-gbp\n[[ $ENABLE_NFP = True ]] \&\& merge_config_group \$TOP_DIR\/local.conf post-config-nfp/' stack.sh
 
